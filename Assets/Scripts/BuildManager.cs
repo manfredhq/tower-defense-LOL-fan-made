f8c6ebed6392a57a15e46041d2607ba73330efd6
manfredhq <hugo.quitton@gmail.com>
2020-04-27 11:55:32 +0200

    turret creation
    added a preview of the node selected, we can create new turrets on each nodes  and not in the top of other turret.

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("too many Build manager");
+            return;
+        }
+        instance = this;
+
+    }
+
+    public GameObject standartTurretPrefab;
+
+    private void Start()
+    {
+        turretToBuild = standartTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject currentTurret;
+
+    private Renderer rend;
+    private Color baseColor;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        baseColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (currentTurret != null)
+        {
+            //TODO: upgrade systeme aswell as a selling one
+            Debug.Log("there is already something here");
+            return;
+        }
+
+        //build a turret
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        currentTurret = Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+
+    }
+
+    private void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = baseColor;
+    }
+}
