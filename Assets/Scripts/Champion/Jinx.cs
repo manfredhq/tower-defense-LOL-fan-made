489cb9f6984f79c17ddb1d51bfa4c1fc92992c06
manfredhq <hugo.quitton@gmail.com>
2020-04-29 17:10:34 +0200

    Jinx Upgraded
    Jinx is working as intended, and added the mafia skin for the upgraded version.

---
+++ Assets/Scripts/Champion/Jinx.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jinx : MonoBehaviour
+{
+    public Turret turret;
+    public float fireRateAugmentRate;
+    public int maxStack;
+
+    private float baseFireRate;
+    private float effectiveFireRate;
+
+    private void Start()
+    {
+        baseFireRate = turret.fireRate;
+    }
+    private void Update()
+    {
+        if (turret.timeShooted > maxStack)
+        {
+            effectiveFireRate = baseFireRate + fireRateAugmentRate * maxStack;
+        }
+        else
+        {
+            effectiveFireRate = baseFireRate + fireRateAugmentRate * turret.timeShooted;
+        }
+        turret.fireRate = effectiveFireRate;
+    }
+
+}
