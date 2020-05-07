5aff2dabc223af8279eb93cab15f822cc81328d0
manfredhq <hugo.quitton@gmail.com>
2020-05-07 22:30:02 +0200

    Revert "Rework/shop"

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -25,16 +25,15 @@ public class BuildManager : MonoBehaviour
 
     private TurretBlueprint turretToBuild;
     private Node selectedNode;
-    private ChampShop champShop;
 
     public bool CanBuild { get { return turretToBuild != null; } }
     public bool HasMoney { get { return PlayerStats.money >= turretToBuild.cost; } }
 
-    public void SelectTurretToBuild(TurretBlueprint turret, ChampShop champShopGiven)
+    public void SelectTurretToBuild(TurretBlueprint turret)
     {
         turretToBuild = turret;
         selectedNode = null;
-        champShop = champShopGiven;
+
         DeselectNode();
     }
 
@@ -46,7 +45,6 @@ public class BuildManager : MonoBehaviour
             return;
         }
         selectedNode = node;
-        champShop = null;
         turretToBuild = null;
 
         turretUI.SetTarget(node);
@@ -61,15 +59,5 @@ public class BuildManager : MonoBehaviour
     public TurretBlueprint GetTurretToBuild()
     {
         return turretToBuild;
-    }
-
-    public ChampShop GetChampShop()
-    {
-        return champShop;
-    }
-
-    public void TurretBuild()
-    {
-        turretToBuild = null;
     }
 }
