5cc30443cd2e53bab9cce06856b3369da462385c
Hugo <hugo.quitton@gmail.com>
2020-05-09 14:36:59 +0200

    Endless Spawner done

--- Assets/Scripts/EndlessWaveSpawner.cs
+++ Assets/Scripts/EndlessWaveSpawner.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class EndlessWaveSpawner : MonoBehaviour
 {
     public int valueWave1 = 10;
     public int valueLastWave;
+    public int valueThisWave;
     public int valueCurrentWave;
+
 
     public static int EnemiesAlives = 0;
     public Transform container;
@@ -23,16 +26,41 @@ public class EndlessWaveSpawner : MonoBehaviour
     public List<EnemyValue> enemies = new List<EnemyValue>();
 
     private List<EnemyValue> enemiesCanSpawn = new List<EnemyValue>();
+    private int waveIndex = 0;
+    public float countdown = 1f;
+    private bool isWaveSpawnedEntierly = false;
 
     private void Start()
     {
+        isWaveSpawnedEntierly = false;
         valueCurrentWave = valueWave1;
+        valueThisWave = valueCurrentWave;
         enemies.Sort(delegate (EnemyValue a, EnemyValue b)
         {
             return (a.value).CompareTo(b.value);
         });
 
         StartCoroutine(SpawnWave());
+    }
+
+    private void Update()
+    {
+        Debug.Log(EnemiesAlives);
+        if (EnemiesAlives > 0)
+        {
+            return;
+        }
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+
+        countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
+
+        waveCountdownTimer.text = string.Format("{0:00.00}", countdown);
     }
 
     IEnumerator SpawnWave()
@@ -51,6 +79,11 @@ public class EndlessWaveSpawner : MonoBehaviour
             }
         } while (enemiesCanSpawn.Count != 0);
 
+        isWaveSpawnedEntierly = true;
+        valueLastWave = valueThisWave;
+        waveIndex++;
+        countdown = timeBetweenWaves;
+        WaveValueGain(valueLastWave);
         Debug.Log("Wave spawned entierly");
     }
 
@@ -58,6 +91,13 @@ public class EndlessWaveSpawner : MonoBehaviour
     {
         Instantiate(enemy.enemyPrefab, spawnPoint.position, spawnPoint.rotation, container);
         valueCurrentWave -= enemy.value;
+        EnemiesAlives++;
+    }
+
+    void WaveValueGain(int lastWaveValue)
+    {
+        valueThisWave = Mathf.RoundToInt(Mathf.Pow(lastWaveValue, 1.5f) + 3);
+        valueCurrentWave = valueThisWave;
     }
 }
 
